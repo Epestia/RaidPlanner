63f2d9fe4b1ac5efdd2fbb99b34372be8f28c311
Epestia <epestia.lumis@hotmail.com>
2025-05-08 02:10:46 +0200

    power point presentation

--- RaidPlanner.Api/obj/Debug/net8.0/RaidPlanner.Api.AssemblyInfo.cs
+++ RaidPlanner.Api/obj/Debug/net8.0/RaidPlanner.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaidPlanner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17507891dcc78fc3f2ca0b68c03a22d281f6223e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253955d232df01e606253b89fa6c58ea9fec69e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaidPlanner.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaidPlanner.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
